6bb43154158f7d9a14708c6a140655997f536a93
Chelsea <chelsea@tripwiretech.com>
2023-10-10 16:21:44 +1300

    Tutorial

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { WebView } from "react-native-webview";
 
 // npx expo install react-native-webview
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      style={styles.container}
+      source={{ uri: "https://forms.gle/y5SA6ZmzpA8wdsq16" }}
+    />
   );
 }
 
@@ -18,5 +19,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    marginVertical: 64,
   },
 });
